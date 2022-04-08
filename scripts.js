84507545546a084e1833606be1ec7664591530e1
robertkarlsson01 <56827248+robertkarlsson01@users.noreply.github.com>
2022-04-08 11:39:41 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2l0YW5zaHUxMyIsImEiOiJja2t4djZvNnAybnAyMnVzMTU3c2Z6d3F5In0.irDdb9UviWNpw-p5veIVwg';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [30.44855512089591, 50.53191144961437],
+zoom: 11.15,
+attributionControl: false
+});
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav);
+
+var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+    });
+    
+map.addControl(directions);
